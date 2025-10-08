8e6e185593382893b2c84f0a8a984f15eb931af5
cnwiio <kritgoodgame@gmail.com>
2025-10-08 11:21:58 +0700

    new cooldown

--- game/Code/Player/Player.cs
+++ game/Code/Player/Player.cs
@@ -34,7 +34,7 @@ namespace game
         private bool _isUsingSkill1 = false;
         private float _skill1Timer = 0f;
         private float _skill1Duration = 0.6f; // match PillarOfLight.Duration
-        public  float Skill1Cooldown = 1f; // in seconds
+        public  float Skill1Cooldown = 5f; // in seconds
         public float _skill1CooldownTimer = 0f;
 
         // Skill2 (ArclightCross) state
@@ -42,7 +42,7 @@ namespace game
         private bool _isUsingSkill2 = false;
         private float _skill2Timer = 0f;
         private float _skill2Duration = 0.5f; // match ArclightCross.Duration
-        public float Skill2Cooldown = 5f; // in seconds
+        public float Skill2Cooldown = 10f; // in seconds
         public float _skill2CooldownTimer = 0f;
         public Texture2D Skill2Texture { get; private set; }
 
