7f52fbd97cd1550c263f818b1ca8f46dda745a56
cnwiio <kritgoodgame@gmail.com>
2025-11-02 15:10:38 +0700

    Fix mionr bug

--- game/Code/Game1.cs
+++ game/Code/Game1.cs
@@ -178,8 +178,8 @@ namespace game
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
+            //if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            //    Exit();
 
             // TODO: Add your update logic here
 
