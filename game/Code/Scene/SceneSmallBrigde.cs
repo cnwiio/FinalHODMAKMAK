ea739faa471a598e70ab0174d8b5bcd3851bfdc6
matchaParfait <winnerz987@gmail.com>
2025-11-02 07:47:25 +0700

    Add pause overlay (currently use Key.U gotta change to esc in future.).

--- game/Code/Scene/SceneSmallBrigde.cs
+++ game/Code/Scene/SceneSmallBrigde.cs
@@ -43,6 +43,11 @@ namespace game
         private PotionHUD _potionHUD;
         private SkillHUD _skillHUD;
 
+        // Pause Screen
+        private PauseScreen pauseScreen;
+        private Texture2D pauseTexture;
+
+
 
         public SceneSmallBrigde(Game game) : base(game)
         {
@@ -80,6 +85,10 @@ namespace game
             _skillHUD = new SkillHUD(player, GraphicsDevice);
             _skillHUD.LoadContent(Content);
 
+            pauseTexture = Content.Load<Texture2D>("HUD/PauseScreen");
+            pauseScreen = new PauseScreen(pauseTexture);
+
+
 
             // Insert collision entities
             foreach (IEntity entity in _collision)
@@ -90,6 +99,25 @@ namespace game
         }
         public override void Update(GameTime gameTime)
         {
+            pauseScreen.Update(gameTime);
+
+            if (pauseScreen.IsPaused)
+            {
+                if (pauseScreen.ResumeClicked)
+                {
+                    pauseScreen.Resume(); // resume game
+                }
+                else if (pauseScreen.MainMenuClicked)
+                {
+                    ScreenManager.LoadScreen(new SceneMenu(Game), new FadeTransition(GraphicsDevice, Color.Black, 1f));
+                }
+                else if (pauseScreen.QuitClicked)
+                {
+                    Game.Exit();
+                }
+                return; // skip gameplay updates while paused
+            }
+
             // Keyboard input
             _oldKs = _ks;
             _ks = Keyboard.GetState();
@@ -174,6 +202,8 @@ namespace game
             _potionHUD.Draw(_spriteBatch);
             _skillHUD.Draw(_spriteBatch);
 
+            // Draw pause screen if paused
+            pauseScreen.Draw(_spriteBatch);
             _spriteBatch.End();
         }
         public override void UnloadContent()
